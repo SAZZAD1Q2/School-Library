f5b68ed4941be182415b17fd2b5668dc2d160e74
SAZZAD1Q2 <kurulia115@gmail.com>
2023-08-29 21:35:36 +0600

    class added

--- person.rb
+++ person.rb
@@ -1,36 +1,25 @@
 class Person
-  def initialize(id, age, name = "Unknown", parent_permission = true)
-    @id = id
+  attr_accessor :name, :age
+  attr_reader :id, :parent_permission
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  # getters
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-  attr_reader :id
-  attr_reader :name
-  attr_reader :age
-  # setters
-  attr_writer :name
-  attr_writer :age
+  private
 
-  # -------------------
+  def generate_id
+    Random.rand(1..10_000)
+  end
 
   def of_age?
     @age >= 18
   end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
 end
-
-# person = Person.new(1,2,"Alice")
-# puts person.can_use_service?
-
-# person_2 = Teacher.new(1,5,"Tonny", false,"teaching")
-
-# puts person_2
