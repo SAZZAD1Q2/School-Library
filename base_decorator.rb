8adf0624e24b6fc65227145fb0730b829b5cfca4
SAZZAD1Q2 <kurulia115@gmail.com>
2023-08-30 23:09:53 +0600

    decorate added

---
+++ base_decorator.rb
@@ -0,0 +1,19 @@
+require_relative './nameable'
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+# Create an instance of the Decorator class
+nameable_instance = Nameable.new # Assume Nameable class exists
+decorator_instance = Decorator.new(nameable_instance)
+
+# Calling the method on the decorator
+decorator_instance.correct_name
