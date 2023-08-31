d319376536acc9b92415e3c388a94c28040c6dc7
SAZZAD1Q2 <kurulia115@gmail.com>
2023-08-31 21:50:46 +0600

    made change

--- student.rb
+++ student.rb
@@ -1,16 +1,14 @@
-require_relative 'person'
-require_relative 'classroom'
+require './person'
 
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
-    '¯\\(ツ)/¯'
+    '¯(ツ)/¯'
   end
 
   def classroom=(classroom)
