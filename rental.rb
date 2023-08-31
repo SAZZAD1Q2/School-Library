0109f32f04fded50150abee5e75bfa12c5e2728b
SAZZAD1Q2 <kurulia115@gmail.com>
2023-08-31 22:35:07 +0600

    linters fixed

--- rental.rb
+++ rental.rb
@@ -1,27 +1,13 @@
-require_relative 'book'
-require_relative 'person'
-
-require 'date'
-
 class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
-    @book = book
-    @person = person
 
+    @book = book
     book.rentals << self
-    person.rentals << self
-  end
 
-  def book=(book)
-    @book = book
-    book.add_rental(self)
-  end
-
-  def person=(person)
     @person = person
-    person.add_rental(self)
+    person.rentals << self
   end
 end
