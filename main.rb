65b4fff0c383f9c9a53102185b987d4cb2b1be2f
SAZZAD1Q2 <kurulia115@gmail.com>
2023-09-01 21:42:31 +0600

    pry testing

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
 
-student = Student.new(1, 16, 'Physics', 'Alice')
+student = Student.new(1, 'Physics', 'Alice')
 teacher = Teacher.new(2, 35, 'Mathematics', 'Mr. Smith')
 
 puts student.name
--- student.rb
+++ student.rb
@@ -1,18 +1,35 @@
-require './person'
+# require './person'
 
+# class Student < Person
+#   attr_reader :classroom
+
+#   def initialize(age, name = 'Unknown', parent_permission: true, classroom)
+#     super(age, name, parent_permission: parent_permission)
+#     @classroom = classroom
+#   end
+
+#   def play_hooky
+#     '¯(ツ)/¯'
+#   end
+
+#   def classroom=(classroom)
+#     @classroom = classroom
+#     classroom.students.push(self) unless classroom.students.include?(self)
+#   end
+# end
+require_relative 'person'
 class Student < Person
-  attr_reader :classroom
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom:, name: 'Unknown')
     super(age, name, parent_permission: parent_permission)
-  end
-
-  def play_hooky
-    '¯(ツ)/¯'
+    @classroom = classroom
   end
 
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
   end
 end
